7026e27d21f070f4a860661b7f9b390289ada736
yejinstar <gyj5586@gmail.com>
2023-06-08 23:55:56 +0900

    Fix: 식당 주문 리스트 정보에 시간정보 추가

--- src/main/java/com/db14/achelin/order/dto/OrderInfoForRestaurant.java
+++ src/main/java/com/db14/achelin/order/dto/OrderInfoForRestaurant.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @NoArgsConstructor
@@ -12,4 +14,5 @@ import lombok.NoArgsConstructor;
 public class OrderInfoForRestaurant {
     private String dishName;
     private Long orderNum;
+    private LocalDateTime orderTime;
 }
